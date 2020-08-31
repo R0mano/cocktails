d6463d3accf90062a099f0cb0c1464aa4423f8d2
romain herry <romainherry@MacBook-Pro.local>
2020-08-31 13:50:24 -0700

    add search filter by ingredient

--- controllers/users.js
+++ controllers/users.js
@@ -11,25 +11,50 @@ module.exports = {
   delete: deleteAll,
 };
 
-function index(req, res, next) {
-  console.log(req.query, "This is req.query==========================================");
-  console.log(req.user, "this is req.user///////////////////////////////////////");
+function index(req, res) {
+  // console.log(req.query, "This is req.query==========================================");
+  // console.log(req.user, "this is req.user///////////////////////////////////////");
   
-  const queryFilter = req.query.filter;
+  // const filterOptions = {
+  //   byDrinkName: 'search.php?s=',
+  //   byIngredientName: 'filter.php?i=',
+  // }
+
+  // const queryFilter = req.query.filter || filterOptions.byDrinkName;
+  // console.log(queryFilter, 'QUERY FILTER');
+  let options;
   const userQuery = req.query.userQuery;
-  const options = {
-    url : `${rootURL}search.php?s=${userQuery}`,
+  // console.log(req.query.filter);
+  if (req.query.filter === 'drinks') {
+    // console.log('We are in drinks');
+    options = {
+      url : `${rootURL}search.php?s=${userQuery}`,
+    }
+  
+  } else if(req.query.filter === 'ingredients') {
+    // console.log('We are in ingredients');
+    options = {
+      url : `${rootURL}filter.php?i=${userQuery}`,
+    }
+  } else {
+    // console.log('no filters');
+    options = {
+      url : `${rootURL}search.php?s=${userQuery}`,
+    }
   }
-
-  const filters = {
-    filterByDrink: 'search.php?s=',
-    filterByIngredient: 'filter.php?i=',
-  }
-
   request(
     options, function(err, response, body) {
-      if (err) {console.log(err);} else {
-      response = JSON.parse(body);
+      // console.log(body, 'bodyyyyyyyyyyyyyyy');
+      if (err) {console.log(err, 'err');} else {
+        // console.log(response, 'responseeeeeeeeeeee');
+        if(body) {
+          response = JSON.parse(body);
+        } else {
+          response = {
+            drinks: [],
+          }
+        }
+      
       console.log(response, 'this is the data retrieved from API');
       res.render("users/index", {
         title: "Cocktails",
@@ -38,6 +63,7 @@ function index(req, res, next) {
     }
     }
     );
+  
 }
 
 function create(req, res) {
@@ -47,33 +73,40 @@ function create(req, res) {
   // User.findById(req.user._id, function(user) {
     const regExIngredient = new RegExp("strIngredient");
     // const regExMesure = new RegExp("strMeasure");
-
-    let drink = new Drink();
-    console.log(drink, "new Drink()");
-    drink.name = newFavDrink.strDrink;
-    drink.image = newFavDrink.strDrinkThumb;
-    drink.instructions = newFavDrink.strInstructions;
-    if (!drink.users.includes(req.user._id)) {
-    drink.users.push(req.user._id);
+  Drink.findOne({name: newFavDrink.strDrink}, function(err, drink) {
+    // console.log(drink, 'drinkkkkkkkkkkkkkkkk');
+    if (drink && !drink.users.includes(req.user._id)) {
+      drink.users.push(req.user._id);
+    } else {
+      drink = new Drink();
+      // console.log(drink, "new Drink()");
+      drink.name = newFavDrink.strDrink;
+      drink.image = newFavDrink.strDrinkThumb;
+      drink.instructions = newFavDrink.strInstructions;
+      if (!drink.users.includes(req.user._id)) {
+      drink.users.push(req.user._id);
+      }
+      for (let key in newFavDrink) {
+        if (regExIngredient.test(key) && newFavDrink[key] ) {
+          drink.ingredients.push({name: newFavDrink[key]});
+          // console.log( newFavDrink[key], 'ingredient names to be added to the newly created drink');
+          // console.log(key,'key');
+          // console.log(regExIngredient.test(key),'regExIngredient.test(key)');
+        } 
+      }
+      drink.ingredients.forEach((ingredient, i) => {
+        ingredient.qty = newFavDrink['strMeasure' + (i + 1).toString()]
+      })
+      console.log(drink, 'drink AFTER populating');
     }
-    for (let key in newFavDrink) {
-      if (regExIngredient.test(key) && newFavDrink[key] ) {
-        drink.ingredients.push({name: newFavDrink[key]});
-        // console.log( newFavDrink[key], 'ingredient names to be added to the newly created drink');
-        // console.log(key,'key');
-        // console.log(regExIngredient.test(key),'regExIngredient.test(key)');
-      } 
-    }
-    drink.ingredients.forEach((ingredient, i) => {
-      ingredient.qty = newFavDrink['strMeasure' + (i + 1).toString()]
-    })
-    console.log(drink, 'drink AFTER populating');
     drink.save(function(err) {
       if (err) {console.log(err)}
       res.redirect('/users/drinks');
     })
     // console.log(drink, 'This is the newly created drink in the user Favorites');
   // })
+  });
+    
 }
 
 function show(req,res) {
